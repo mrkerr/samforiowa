1bbae4d41facc6221fea7aa7ecce910fe510c6c2
Mitchell Kerr <mkerr@vailsys.com>
2020-03-19 23:23:18 -0500

    Updates

---
+++ precache-manifest.0fb8222a993e1829b2423e58d37e326c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038faf54913331a064be1530d33cb604",
+    "url": "/samforiowa/index.html"
+  },
+  {
+    "revision": "bb78fec4a1be23fc12bc",
+    "url": "/samforiowa/static/css/main.b9bc1017.chunk.css"
+  },
+  {
+    "revision": "b914b8e12cff77a0da00",
+    "url": "/samforiowa/static/js/2.c65633f0.chunk.js"
+  },
+  {
+    "revision": "fdc84dfb581e6fb8755dd1b2b01a50e1",
+    "url": "/samforiowa/static/js/2.c65633f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb78fec4a1be23fc12bc",
+    "url": "/samforiowa/static/js/main.a4d53c76.chunk.js"
+  },
+  {
+    "revision": "09628d86271c71dcff55",
+    "url": "/samforiowa/static/js/runtime-main.948a519b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/samforiowa/static/media/logo.5d5d9eef.svg"
+  }
+]);
