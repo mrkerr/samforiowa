c59145ee86002e0874e9aa8639283d0c8e5d2a22
Mitchell Kerr <mkerr@vailsys.com>
2020-03-20 00:31:01 -0500

    site is live

--- src/constants/LandingPageConstants.js
+++ src/constants/LandingPageConstants.js
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export const LANDING_PAGE_TEXT = <p>In the meantime, &nbsp;follow along on the <a href='https://www.facebook.com/samforiowa/' target='_blank'>Sam For Iowa</a> Facebook page</p>
+export const LANDING_PAGE_TEXT = <p>In the meantime, &nbsp;follow along on the <a href='https://www.facebook.com/samforiowa/' target='_blank'>Sam For Iowa House</a> Facebook page</p>
