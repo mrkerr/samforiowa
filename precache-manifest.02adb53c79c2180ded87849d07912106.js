b72c255b10c34bc33137ee88ae2d6ed1575d8c76
Mitchell Kerr <mkerr@vailsys.com>
2020-03-20 00:07:39 -0500

    Updates

---
+++ precache-manifest.02adb53c79c2180ded87849d07912106.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae6d15592aa6b169346b2f5b45042af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "667c529752df3457c918",
+    "url": "/static/css/main.b9bc1017.chunk.css"
+  },
+  {
+    "revision": "b914b8e12cff77a0da00",
+    "url": "/static/js/2.c65633f0.chunk.js"
+  },
+  {
+    "revision": "fdc84dfb581e6fb8755dd1b2b01a50e1",
+    "url": "/static/js/2.c65633f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "667c529752df3457c918",
+    "url": "/static/js/main.99da0c20.chunk.js"
+  },
+  {
+    "revision": "5bcf2569b8cb9509bbb2",
+    "url": "/static/js/runtime-main.8e21df3a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/samforiowa/precache-manifest.8bb8eb535b6c122253f61c7465e2df77.js"
+  "/precache-manifest.02adb53c79c2180ded87849d07912106.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/samforiowa/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
